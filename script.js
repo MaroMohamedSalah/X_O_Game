72cb33a51ec07e431e3cd11f405cb6915c28d25c
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2022-08-28 17:39:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,12 +48,6 @@ let changeUserName = document.getElementById('change');
 let reGame = document.getElementById('reGame');
 //sign up
     // localStorage.setItem('username' , '');
-    names.innerHTML = `
-    <h1><span>${localStorage.getItem('username').split(" ")[0]}</span> VS <span><input id="playerName" type="text" placeholder="Enter player name"></span></h1>
-    `
-    names2.innerHTML = `
-    <h1><span><input id="playerName1" type="text" placeholder="Enter player1 name"</span> VS <span><input id="playerName2" type="text" placeholder="Enter player2 name"></span></h1>
-    `
     if(localStorage.getItem('username') === ''){
         signUp.style.display = 'block';
         who.style.display = 'none'
